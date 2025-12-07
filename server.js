567c48e34511e0a24e942761597932fa6e2d535c
vertigorestore-byte <vertigorestore@gmail.com>
2025-12-07 14:52:42 +1000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 8080 });
+const rooms = {};
+
+wss.on("connection", ws => {
+
+    ws.on("message", msg => {
+        const packet = JSON.parse(msg);
+
+        if (packet.type === "join") {
+            if (!rooms[packet.roomId]) rooms[packet.roomId] = [];
+            rooms[packet.roomId].push(ws);
+            return;
+        }
+
+        if (packet.type === "msg") {
+            const clients = rooms[packet.roomId] || [];
+            clients.forEach(client => {
+                if (client !== ws && client.readyState === WebSocket.OPEN) {
+                    client.send(JSON.stringify({
+                        type:"msg",
+                        data: packet.data
+                    }));
+                }
+            });
+        }
+    });
+
+    ws.on("close", () => {
+        for (const room in rooms) {
+            rooms[room] = rooms[room].filter(c => c !== ws);
+        }
+    });
+});
+
+console.log("Secure Relay Server running on ws://192.168.1.2:8080");
